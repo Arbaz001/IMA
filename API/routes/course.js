b09f4232e6f21ab03aeb24338cb4c81dcedc1d52
Arbaz001 <arbaza3212btechcse2025@kccitm.edu.in>
2025-01-31 16:31:59 +0530

    Course Api Done

--- API/routes/course.js
+++ API/routes/course.js
@@ -1,13 +1,204 @@
 const express = require('express')
 const router = express.Router()
 const checkAuth = require('../middleware/checkAuth')
+const Course = require('../model/Course')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+   cloud_name: process.env.CLOUD_NAME,
+   api_key: process.env.API_KEY,
+   api_secret: process.env.API_SECRET,
+})
  
-//add new course routes
+//add new course routes ki API routes
 router.post('/add-course',checkAuth,(req, res)=>{
-   // yeaha se start karna hai 2:57:30
+   //is token se humne userId nikal liya
+      const token = req.headers.authorization.split(' ')[1]
+      const verify = jwt.verify(token,process.env.SECRET_KEY)
+   //token milne ke baad ab applicble hau as user ki hum course add karenge
+   // yeha image upload hoga
+      cloudinary.uploader.upload(req.files.image.tempFilePath, (err, result) => {
+         //yeha course details upload hoga
+         const newCourse = new Course({
+            _id: new mongoose.Types.ObjectId(),
+            courseName:req.body.courseName,
+            price:req.body.price,
+            description:req.body.description,
+            startingDate:req.body.startingDate,
+            endDate:req.body.endDate,
+            uId:verify.uId,
+            imageId:result.public_id,
+            imageUrl:result.secure_url
+         })
+         // yeah save hojayega
+         newCourse.save() 
+         // save hone ke baad massage milega ki course added succefully
+         .then(result => {
+            res.status(200).json({
+               message:"Course added successfully",newCourse:result
+            })
+         })
+         // agar nhi add hua to error aa jayega
+         .catch(err => {
+            console.log(err)
+            res.status(500).json({
+               error:err
+            })
+         })
+      })
+   })
+
+   //get all courses routes ki API routes
+router.get('/all-courses',checkAuth,(req, res)=>{
+   //is token se humne userId nikal liya
+      const token = req.headers.authorization.split(' ')[1]
+      const verify = jwt.verify(token,process.env.SECRET_KEY)
+   //token milne ke baad ab applicble hau as user ki hum course get karenge
+   // yeha course details fetch hoga
+      Course.find({uId:verify.uId})
+      // hume jo dekhana respone me uske liye select() ka use karenge
+      .select('_id uId courseName description price startingDate endDate imageUrl imageId')
+      .then(courses => {
+         res.status(200).json({
+            message:"All courses fetched successfully",courses:courses
+         })
+      })
+      // agar nhi fetch hua to error aa jayega
+      .catch(err => {
+         console.log(err)
+         res.status(500).json({
+            error:err
+         })
+      })
 })
 
+//get single course routes ki API routes
+router.get('/course-detail/:id',checkAuth,(req, res)=>{
+   
+      Course.findById(req.params.id)
+     
+      .select('_id uId courseName description price startingDate endDate imageUrl imageId')
+      .then(result => {
+         res.status(200).json({
+            message:"Course fetched successfully",course:result
+         })
+      })
+      .catch(err => {
+         console.log(err)
+         res.status(500).json({
+            error:err
+         })
+      })
+   })
+
+//delete the course
+   router.delete('/:id',checkAuth,(req, res)=>{
+      const token = req.headers.authorization.split(' ')[1]
+      const verify = jwt.verify(token,process.env.SECRET_KEY)
+
+      Course.findByIdAndDelete(req.params.id)
+      .then(course => {
+         console.log(course)
+         if(course.uId == verify.uId) 
+         {
+           Course.findByIdAndDelete(req.params.id)  //delete course from the database
+            .then(result => {
+               cloudinary.uploader.destroy(course.imageId,(deletedImage)=>{  // here deleting the image on cloudinary server
+                  res.status(200).json({
+                     message:"Course deleted successfully"
+                  })
+               })
+             })
+             .catch(err => {
+                console.log(err)
+                res.status(500).json({
+                   msg:err
+                })
+             })
+         }
+         else{
+            res.status(401).json({
+               message:"Unauthorized to delete this course"
+            })
+         }
+      })   
+   })
+
+   // update course
+   router.put('/:id',checkAuth,(req, res)=>{
+      
+      const token = req.headers.authorization.split(' ')[1]
+      const verify = jwt.verify(token,process.env.SECRET_KEY)
+
+      Course.findById(req.params.id)
+      .then(course=>{
+         if(course.uId != verify.uId){
+            return res.status(500).json({message:"Unauthorized to update this course"})
+         }
+         // agar image file saath me hua to update karne ke liye
+         if(req.files){
+            cloudinary.uploader.destroy(course.imageId,(deletedImage)=>{  // here deleting the image on cloudinary server
+               cloudinary.uploader.upload(req.files.image.tempFilePath, (err, result) => {
+                  //yeha course details upload hoga
+                  const NewupdatedCoursef = {
+                     courseName:req.body.courseName,
+                     price:req.body.price,
+                     description:req.body.description,
+                     startingDate:req.body.startingDate,
+                     endDate:req.body.endDate,
+                     uId:verify.uId,
+                     imageId:result.public_id,
+                     imageUrl:result.secure_url
+                  }
+                  Course.findByIdAndUpdate(req.params.id,NewupdatedCoursef,{new:true})
+                  .then(data=>{
+                     res.status(200).json({
+                        message:"Course updated successfully",NewupdatedCoursef:data
+                     })
+                  })
+                  .catch(err=>{
+                     res.status(500).json({
+                        message:err.message
+                     })
+                  })
+               })
+            })
+         }
+         // agar image file nahi hua to update karenge
+         else{
+            const NewupdatedCoursenf={
+               courseName:req.body.courseName,
+               price:req.body.price,
+               description:req.body.description,
+               startingDate:req.body.startingDate,
+               endDate:req.body.endDate,
+               uId:verify.uId,
+               imageUrl:course.imageUrl,
+               imageId:course.imageId
+            }
+            Course.findByIdAndUpdate(req.params.id,NewupdatedCoursenf,{new:true})
+            .then(data=>{
+               res.status(200).json({
+                  message:"Course updated successfully",NewupdatedCoursenf:data
+               })
+            })
+            .catch(err=>{
+               res.status(500).json({
+                  message:err.message
+               })
+            })
+         }
+
+      })
+      .catch(err=>{
+         res.status(500).json({
+            message:err.message
+         })
+      })
+   })
+   
 
 
-
-module.exports =router
+module.exports = router
