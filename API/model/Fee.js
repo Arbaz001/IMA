aca5a74de23037642e62eeab0c677f790e2bb65e
Arbaz001 <arbaza3212btechcse2025@kccitm.edu.in>
2025-02-01 13:10:06 +0530

    Fee api work done and some more api done in future according to need extra features on backend.

---
+++ API/model/Fee.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+
+// Fee Schema
+const feeSchema = new mongoose.Schema({
+    _id:mongoose.Types.ObjectId,
+   fullName:{type:String,required:true},
+   phone:{type:String,required:true,unique:true},
+   courseId:{type:String,required:true},
+   uId:{type:String, required:true},
+   amount:{type:Number,required:true},
+   remark:{type:String,required:true},
+},{timestamps:true})
+
+module.exports = mongoose.model('Fee',feeSchema)
--- API/routes/fee.js
+++ API/routes/fee.js
@@ -1,15 +1,71 @@
 const express = require('express')
+const checkAuth = require('../middleware/checkAuth')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
+const Fee = require('../model/Fee')
+const mongoose = require('mongoose')
 
- 
-router.post('/add-fee',(req, res)=>{
-    res.status(200).json({
-        msg:"add new fee request "
+ //Add fee
+router.post('/add-fee',checkAuth,(req, res)=>{
+    const token = req.headers.authorization.split(' ')[1]
+    const verify = jwt.verify(token,process.env.SECRET_KEY)
+
+    const newFee = new Fee({
+        _id: new mongoose.Types.ObjectId(),
+        fullName:req.body.fullName,
+        phone:req.body.phone,
+        courseId:req.body.courseId,
+        uId:verify.uId,
+        amount:req.body.amount,
+        remark:req.body.remark,
+    })
+    newFee.save()
+    .then(result=>{
+        res.status(200).json({
+            newFee:result
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error:err
+        })
     })
 })
 
+//get all fee collection data for any user
+router.get('/payment-history',checkAuth,(req,res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    const verify = jwt.verify(token,process.env.SECRET_KEY)
 
+    Fee.find({uId:verify.uId})
+    .then(result=>{
+        res.status(200).json({
+            paymentHistory:result
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            error:err
+        })
+    })
+})
 
+//get all payments for any students in a course
+router.get('/all-payments',checkAuth,(req,res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    const verify = jwt.verify(token,process.env.SECRET_KEY)
 
+    Fee.find({courseId:req.query.courseId,uId:verify.uId,phone:req.query.phone})
+    .then(result=>{
+        res.status(200).json({
+            coursePayments:result
+        })
+    })
+   .catch(err=>{
+        res.status(500).json({
+            error:err
+        })
+    })
+})
 
 module.exports =router
